6ff7c8582aba347485a5a71ee9022902b6544619
dpberry <dpberry178@gmail.com>
2018-02-19 17:05:43 -0500

    Added RouteMapper and CustomerController

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetNuke.Web.Api;
+using System.Web.Http;
+using HTC_CRM_DataAccess.Models;
+using HTC_CRM_DataAccess.Connections;
+
+namespace AAModules.HTCAPI.Controllers
+{
+    public class CustomerController : DnnApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            using(var db = DBConnection.GetConnection())
+            {
+                return Customer.GetAll<Customer>(db);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public Customer Get(int id)
+        {
+            using(var db = DBConnection.GetConnection())
+            {
+                return Customer.GetById<Customer>(db, id);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        public bool Put([FromBody] Customer c)
+        {
+            using(var db = DBConnection.GetConnection())
+            {
+                return Customer.Persist<Customer>(db, c);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public bool Post([FromBody] Customer c)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return Customer.Persist<Customer>(db, c);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpDelete]
+        public bool Delete(Customer c)
+        {
+            using(var db = DBConnection.GetConnection())
+            {
+                return Customer.Delete<Customer>(db, c);
+            }
+        }
+    }
+}
