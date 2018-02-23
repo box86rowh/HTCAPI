f8c68d5c86c9aa672e8efd56c130663f8ea36783
dpberry <dpberry178@gmail.com>
2018-02-23 18:17:52 -0500

    Adding controllers

---
+++ Controllers/JobNoteController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using DotNetNuke.Web.Api;
+using System.Web.Http;
+using HTC_CRM_DataAccess.Models;
+using HTC_CRM_DataAccess.Connections;
+namespace AAModules.HTCAPI.HTCAPI.Controllers
+{
+    public class JobNoteController : DnnApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<JobNote> Get()
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return JobNote.GetAll<JobNote>(db);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public JobNote Get(int id)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return JobNote.GetById<JobNote>(db, id);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        public bool Put([FromBody] JobNote j)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return JobNote.Persist<JobNote>(db, j);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public bool Post([FromBody] JobNote j)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return JobNote.Persist<JobNote>(db, j);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpDelete]
+        public bool Delete(JobNote j)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return JobNote.Delete<JobNote>(db, j);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<JobNote> GetByParentId(int id)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return JobNote.GetByJobId(db, id);
+            }
+        }
+    }
+}
---
+++ Controllers/SubContractorJobController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using DotNetNuke.Web.Api;
+using System.Web.Http;
+using HTC_CRM_DataAccess.Models;
+using HTC_CRM_DataAccess.Connections;
+
+namespace AAModules.HTCAPI.Controllers
+{
+    public class SubContractorJobController : DnnApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<SubContractorJob> Get()
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return SubContractorJob.GetAll<SubContractorJob>(db);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public SubContractorJob Get(int id)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return SubContractorJob.GetById<SubContractorJob>(db, id);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPut]
+        public bool Put([FromBody] SubContractorJob c)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return SubContractorJob.Persist<SubContractorJob>(db, c);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public bool Post([FromBody] SubContractorJob c)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return SubContractorJob.Persist<SubContractorJob>(db, c);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpDelete]
+        public bool Delete(SubContractorJob c)
+        {
+            using (var db = DBConnection.GetConnection())
+            {
+                return SubContractorJob.Delete<SubContractorJob>(db, c);
+            }
+        }
+    }
+}
